f84ec08c40cbac924e49f428a8a8f6fe1a9c8489
Mirko Siddi <mirko.siddi@gmail.com>
2020-12-14 19:41:54 +0100

    Deploy 141220:19:33

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(
     credentials: true,
     origin: [
       "http://localhost:3000",
-      "http://lmen-confeccion.herokuapp.com",
+      "https://lmen-confeccion.herokuapp.com",
       "https://lmen-confeccion.herokuapp.com",
     ],
   })
